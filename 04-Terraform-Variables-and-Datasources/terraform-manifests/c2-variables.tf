be1edae79de47223db51b6732c2f0b089c4b25bd
Nishitha1992 <nishitha.v1192@gmail.com>
2023-12-29 18:14:53 +0530

    key-removal

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
@@ -13,11 +13,5 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
-# AWS EC2 Instance Key Pair
-variable "instance_keypair" {
-  description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
-  type = string
-  default = "terraform-key"
-}
 
 
